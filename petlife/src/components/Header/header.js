36639e6d7449317457f953b3e574c7e0dbaa65f3
Chika <catherine.achukwu@gmail.com>
2019-09-20 14:07:26 -0400

    sidebar created

--- petlife/src/components/Header/header.js
+++ petlife/src/components/Header/header.js
@@ -1,17 +1,17 @@
 import React from "react";
 import "./header.scss";
 
-function Header() {
+function Header(prop) {
   return (
     <header className="Header">
       <div className="row">
         <div className="col-3 logo">
           <img src="./logo.png" alt="Pet Life Logo" />
         </div>
-        <div className="col-9 text-right navbar">Navbar</div>
+        <div className="col-9 text-right navbar">Welcome{prop.userName}</div>
       </div>
     </header>
-  );
+  ); 
 }
 
 export default Header;

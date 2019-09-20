921c6278646b12dd761b21b2656e411fa60fda33
Stephen Allen <staylorallen@gmail.com>
2019-09-20 14:31:33 -0400

    stable. protected routes working

--- petlife/customMiddleware/authware.js
+++ petlife/customMiddleware/authware.js
@@ -5,7 +5,7 @@ module.exports = function(req, res, next) {
   try{
     const {authorization} = req.headers;
     if (!authorization) throw new Error();
-    const token = authorization.replace("Bearer " + "");
+    const token = authorization.replace("Bearer ", "");
     const decoded = jwt.verify(token, "secretKey");
 
     User.findOne({ _id: decoded.data }).then(function(dbUser) {
@@ -13,6 +13,6 @@ module.exports = function(req, res, next) {
       next();
     });
   } catch(err) {
-    res.status(401).json({message: "Unauthorized"});
+    res.status(401).json({message: err});
   }
 };
--- petlife/routes/api-routes.js
+++ petlife/routes/api-routes.js
@@ -1,6 +1,6 @@
 const User = require("../models/User");
 const jwt = require("jsonwebtoken");
-const authWare = require ("../customMiddleware/authware");
+const authWare = require("../customMiddleware/authware");
 
 module.exports = function(app) {
 
@@ -10,43 +10,62 @@ module.exports = function(app) {
   // json message saying user created.
   // if error, send error.
   app.post("/api/signup", function(req, res) {
+    console.log(req.body);
     User.create(req.body)
-    .then(function(result) {
-      res.json({message: 'user created' });
-    }).catch(function(err) {
-      res.status(500).json({error: err.message});
-    });
+      .then(function(result) {
+        res.json({
+          message: 'user created'
+        });
+      }).catch(function(err) {
+        res.status(500).json({
+          error: err.message
+        });
+      });
   });
 
 
-// post requests to see if the user is authenticated.
-app.post("/api/authenticate", function(req, res) {
-  const {username, password} = req.body;
-  User.findOne({username: username})
-  .then(function(dbUser) {
-    if(!dbUser) return res.status(401).json({message:"Username and or password is incorrect"});
-    if (dbUser.comparePassword(password)){
+  // post requests to see if the user is authenticated.
+  app.post("/api/authenticate", function(req, res) {
+    console.log(req.body);
+    const {
+      username,
+      password
+    } = req.body;
+    User.findOne({
+        username: username
+      })
+      .then(function(dbUser) {
+        if (!dbUser) return res.status(401).json({
+          message: "Username and or password is incorrect"
+        });
+        if (dbUser.comparePassword(password)) {
 
-      const token = jwt.sign({data:dbUser._id}, "secretKey");
+          const token = jwt.sign({
+            data: dbUser._id
+          }, "secretKey");
 
-      res.json({
-        id: dbUser._id,
-        username: dbUser.username,
-        token:token
-      });
-    } else {
-      res.status(401).json({message:"Username and or password is incorrect"});
-    }
-  })
-});
+          res.json({
+            id: dbUser._id,
+            username: dbUser.username,
+            token: token
+          });
+        } else {
+          res.status(401).json({
+            message: "Username and or password is incorrect"
+          });
+        }
+      })
+  });
 
 
-// testing protected routes. uses custom authWare middle ware to
-// check if the user is authenticated. 
-app.get("/api/protected", authWare, function(req, res) {
-  const user = req.user;
-  res.json({message: user.username + ", should be protected"});
-});
+  // testing protected routes. uses custom authWare middle ware to
+  // check if the user is authenticated.
+  app.get("/api/protected", authWare, function(req, res) {
+    const user = req.user;
+    res.json({
+      message: user.username + ", should be protected"
+    });
+  });
 
 
 

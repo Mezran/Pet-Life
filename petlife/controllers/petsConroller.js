af071a5692422a36c4580e06edcdf233d84d773c
Chika <catherine.achukwu@gmail.com>
2019-09-20 13:33:43 -0400

    controllers created for pets and user models

---
+++ petlife/controllers/petsConroller.js
@@ -0,0 +1,36 @@
+const db = require("../models");
+
+// Defining methods for the booksController
+module.exports = {
+    findAll: function (req, res) {
+        db.Pet
+            .find(req.query)
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err));
+    },
+    findById: function (req, res) {
+        db.Pet
+            .findById(req.params.id)
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err));
+    },
+    create: function (req, res) {
+        db.Pet
+            .create(req.body)
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err));
+    },
+    update: function (req, res) {
+        db.Pet
+            .findOneAndUpdate({ _id: req.params.id }, req.body)
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err));
+    },
+    remove: function (req, res) {
+        db.Pet
+            .findById({ _id: req.params.id })
+            .then(dbModel => dbModel.remove())
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err));
+    }
+};

6e46406b7a688c659da84ee1e00005fe9ce98e33
Chika <catherine.achukwu@gmail.com>
2019-09-21 12:21:10 -0400

    changes to side bar

--- petlife/src/components/Sidebar/sidebar.js
+++ petlife/src/components/Sidebar/sidebar.js
@@ -1,27 +1,28 @@
 import React from "react";
 import "./sidebar.scss";
+import { NavLink } from "react-router-dom";
 
 function Sidebar() {
   return (
     <div id="sidebar" className="col-3">
 
-      <ul class="nav flex-column">
-        <li class="nav-item">
-          <a class="nav-link active" href="petFamily">Pet Family</a>
+      <ul className="nav flex-column">
+        <li className="nav-item">
+          <NavLink className="nav-link" to="/petFamily">Pet Family</NavLink>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/petInfo">Pet Info</a>
+        <li className="nav-item">
+          <NavLink className="nav-link" to="/petInfo">Pet Info</NavLink>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/prescription">Prescription</a>
+        <li className="nav-item">
+          <NavLink className="nav-link" to="/prescription">Prescription</NavLink>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/doctorsVisits">Doctors Visits</a>
+        <li className="nav-item">
+          <NavLink className="nav-link" to="/doctorsVisits"> Visits</NavLink>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/petSitter">Pet Sitter</a>
+        <li className="nav-item">
+          <NavLink className="nav-link" to="/petSitter">Pet Sitter</NavLink>
         </li>
-        
+
       </ul>
     </div>
   );

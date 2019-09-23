8a92440ceb42838d17506ee41df600841ffee985
Zigy <zigybass@gmail.com>
2019-09-23 12:47:31 -0400

    some styling added, jumbo included. linked file to Router

--- client/src/pages/PetInfo.js
+++ client/src/pages/PetInfo.js
@@ -1,10 +1,20 @@
 import React from "react";
+import "./petInfo.css"
 
 function PetInfo() {
   return (
+      <>
     <div className="container">
       <h1>Pet Info</h1>
     </div>
+    <div className="jumbotron petInfoJumbo">
+        <p>Name:</p>
+        <p>Nicknames:</p>
+        <p>Birthday:</p>
+        <p>Diet:</p>
+        <p>Prescriptions:</p>
+    </div>
+    </>
   );
 }
 

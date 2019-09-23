f68319cca4094fd425336f9c8149d16dcfc2c584
Zigy <zigybass@gmail.com>
2019-09-23 14:02:04 -0400

    final edition pet info

--- client/src/pages/PetInfo.js
+++ client/src/pages/PetInfo.js
@@ -1,20 +1,25 @@
 import React from "react";
-import "./petInfo.css"
+import "./petInfo.css";
 
 function PetInfo() {
   return (
-      <>
-    <div className="container">
-      <h1>Pet Info</h1>
-    </div>
-    <div className="jumbotron petInfoJumbo">
-        <p>Name:</p>
-        <p>Nicknames:</p>
-        <p>Birthday:</p>
-        <p>Diet:</p>
-        <p>Prescriptions:</p>
-    </div>
-    </>
+    <div className="petInfoCont">
+      <div className="container">
+        <h1>Pet Info</h1>
+      </div>
+      <div className="jumbotron petInfoJumbo">
+        <div className="col text-right petInfoImage">
+            <h2>Image</h2> 
+        </div>
+        <div className="col text-start">   
+          <p>Name:</p>
+          <p>Nicknames:</p>
+          <p>Birthday:</p>
+          <p>Prescriptions:</p>
+          <p>Diet:</p>
+        </div>
+      </div>
+      </div>
   );
 }
 

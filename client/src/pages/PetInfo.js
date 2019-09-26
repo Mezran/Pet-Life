7e682c9ef3a3a3103717f63e239af1296c5d0a6a
coti_garcia <javiera.garcialatorre@gmail.com>
2019-09-26 11:43:00 -0400

    commit

--- client/src/pages/PetInfo.js
+++ client/src/pages/PetInfo.js
@@ -1,54 +1,53 @@
 import React from "react";
 import API from "../utils/API2";
 import axios from "axios";
-import PetInfoCard from "../components/petInfoCard/petInfoCard"
+import PetInfoCard from "../components/petInfoCard/petInfoCard";
 
 class PetInfo extends React.Component {
-
   state = {
     petName: "",
     nickname: "",
     breed: "",
     birthday: "",
-    allergies: "", 
-    temperament: "", 
+    allergies: "",
+    temperament: "",
     diet: "",
     directions: "",
-    id: "5d8c2b38e773341971f43e8c"
+    id: " "
+  };
 
-};
-
-componentDidMount () {
-  axios.get("/api/getPets/" + this.state.id).then( data => {
-    console.log(data)
-    this.setState({ 
-      petName: data.data.name,
-      breed: data.data.breed,
-      diet: data.data.diet,
-      birthday: data.data.birthday.slice(0, -14),
-      temperament: data.data.temperament,
-      nickname: data.data.nicknames })
-  })
-}
+  componentDidMount() {
+    axios.get("/api/getPets/" + this.state.id).then(data => {
+      console.log(data);
+      this.setState({
+        // petName: data.data.name,
+        // breed: data.data.breed,
+        // diet: data.data.diet,
+        // birthday: data.data.birthday.slice(0, -14),
+        // temperament: data.data.temperament,
+        // nickname: data.data.nicknames
+      });
+    });
+  }
 
   render() {
-  return (
-    <div className="petInfoCont">
-      <div className="container">
-        <h1>Pet Info</h1>
+    return (
+      <div className="petInfoCont">
+        <div className="container">
+          <h1>Pet Info</h1>
+        </div>
+        <PetInfoCard
+          img="https://via.placeholder.com/150"
+          name={this.state.petName}
+          breed={this.state.breed}
+          nickname={this.state.petName}
+          birthday={this.state.birthday}
+          temperament={this.state.temperament}
+          diet={this.state.diet}
+        />
       </div>
-      <PetInfoCard
-        img="https://via.placeholder.com/150"
-        name={this.state.petName}
-        breed={this.state.breed}
-        nickname={this.state.petName}
-        birthday= {this.state.birthday}
-        temperament={this.state.temperament}
-        diet={this.state.diet}
-      />
-    </div>
-  );
-}
+    );
+  }
 }
 
 export default PetInfo;

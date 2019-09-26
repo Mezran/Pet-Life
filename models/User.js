de0a158653ee7c78f105596f1c57eefa28a4f648
coti_garcia <javiera.garcialatorre@gmail.com>
2019-09-26 16:13:57 -0400

    user has pet as empty arrey

--- models/User.js
+++ models/User.js
@@ -1,16 +1,21 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 const Schema = mongoose.Schema;
 
-const UserSchema = new mongoose.Schema ({
+const UserSchema = new mongoose.Schema({
   username: {
     type: String,
     index: {
-      unique:true
+      unique: true
     }
   },
   password: String,
-  pets: Array,
+  pets: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Pet"
+    }
+  ],
   petSitters: [
     {
       type: Schema.Types.ObjectId,
@@ -21,12 +26,12 @@ const UserSchema = new mongoose.Schema ({
 
 UserSchema.methods.comparePassword = function(inputPass) {
   return bcrypt.compareSync(inputPass, this.password);
-}
+};
 
-UserSchema.pre('save', function(next) {
-  if(!this.isModified('password')) return next();
+UserSchema.pre("save", function(next) {
+  if (!this.isModified("password")) return next();
   this.password = bcrypt.hashSync(this.password, 10);
   return next();
-})
+});
 
 module.exports = mongoose.model("User", UserSchema);

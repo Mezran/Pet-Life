3c994b48887fbaf71d3e4d2f37237e6072769e26
coti_garcia <javiera.garcialatorre@gmail.com>
2019-09-29 19:55:11 -0400

    pet info style ok

--- client/src/components/Family/index.js
+++ client/src/components/Family/index.js
@@ -1,32 +1,35 @@
 import React from "react";
 import "./style.scss";
 import { Link } from "react-router-dom";
-import UserContext from "../../context/UserContext"
-
+import UserContext from "../../context/UserContext";
 
 function Pet(props) {
   return (
     <UserContext.Consumer>
-      
-      { ({user}) => ( 
-      <button className="familyCard">
-        <Link to={`/user/${user.id}/pets`}>
-          <div>
-            <img
-              src={props.img || "https://via.placeholder.com/150"}
-              className="card-img-top familyCard"
-              alt="first pet"
-            />
-            <div className="card-body famName">
-              <p className="card-text">{props.name}</p>
-            </div>
+      {({ user }) => (
+        <div className="col-12 col-sm-3 ">
+          <div className="familyCard text-center">
+            <Link to={`/user/${user.id}/pets`}>
+              <div>
+                <div className="petImg">
+                  <img
+                    src={
+                      props.img ||
+                      "https://www.missingdogsuk.co.uk/wp-content/plugins/wp-job-manager-resumes/assets/images/candidate.png"
+                    }
+                    className="card-img-top familyCard"
+                    alt="first pet"
+                  />
+                </div>
+                <div className="famName">
+                  <p>{props.name}</p>
+                </div>
+              </div>
+            </Link>
           </div>
-        </Link>
-      </button>
-      
-      )
-    }
-  </UserContext.Consumer>
+        </div>
+      )}
+    </UserContext.Consumer>
   );
 }
 

0301933bf0c9ccc959c17740cea4729fc6ded6a5
coti_garcia <javiera.garcialatorre@gmail.com>
2019-09-29 20:39:32 -0400

    PetInfo / Pet Sitter / add detial ok

--- client/src/pages/PetInfo.js
+++ client/src/pages/PetInfo.js
@@ -2,13 +2,15 @@ import React from "react";
 // import API from "../utils/API2";
 import axios from "axios";
 import PetInfoCard from "../components/petInfoCard/petInfoCard";
-import UserContext from "../context/UserContext"
+import UserContext from "../context/UserContext";
 
 class PetInfo extends React.Component {
-static contextType = UserContext;
+  static contextType = UserContext;
 
   state = {
-    pets: []
+    pets: [],
+    mounted: false,
+    refreshed: false
   };
 
   componentDidMount() {
@@ -21,41 +23,60 @@ static contextType = UserContext;
     });
   }
 
-  deletePet = (petId) => {
-    axios.delete(`/api/user/${petId}/petFamily`).then(function (res) {
-      console.log("pet deleted")
-      });
-      let currentComponent = this;
-      axios.get(`/api/user/${this.context.user.id}/petFamily`).then(function (res) {
+  componentDidUpdate() {
+    if (this.state.mounted === false) {
+      if (this.state.refreshed === false) {
+        axios.get(`/api/user/${this.context.user.id}/petFamily`).then(res => {
+          console.log(res.data);
+          this.setState({
+            pets: res.data.pets,
+            refreshed: true
+          });
+        });
+      } else {
+        this.setState({
+          mounted: true
+        });
+      }
+    }
+  }
+
+  deletePet = petId => {
+    axios.delete(`/api/user/${petId}/petFamily`).then(function(res) {
+      console.log("pet deleted");
+    });
+    let currentComponent = this;
+    axios
+      .get(`/api/user/${this.context.user.id}/petFamily`)
+      .then(function(res) {
         console.log(res.data);
         currentComponent.setState({
           pets: res.data.pets
         });
       });
- };
+  };
 
-  render() { 
+  render() {
     return (
       <div className="petInfoCont">
         <div className="container">
           <h1>Pet Info</h1>
         </div>
-        {this.state.pets.map( item => (
+        {this.state.pets.map(item => (
           <PetInfoCard
-          key={item._id}
-          id={item._id}
-          img={item.image}
-          name={item.name}
-          breed={item.breed}
-          nickname={item.nicknames}
-          birthday={item.birthday}
-          temperament={item.temperament}
-          diet={item.diet}
-          directions={item.directions}
-          deletePetCB={this.deletePet}
-        />
-          )
-        )}
+            key={item._id}
+            id={item._id}
+            img={item.image}
+            name={item.name}
+            breed={item.breed}
+            nickname={item.nicknames}
+            birthday={item.birthday}
+            temperament={item.temperament}
+            diet={item.diet}
+            directions={item.directions}
+            deletePetCB={this.deletePet}
+          />
+        ))}
       </div>
     );
   }
